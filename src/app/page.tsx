c5bda53a6f02522f0603e2af4d77fffca3bbe43a
ashmit singh <ashmitsingh1310@gmail.com>
2025-09-12 14:34:03 +0530

    Add New Features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "@/components/sections/HeroSection";
 import FaqSection from "@/components/sections/FaqSection";
-import Testimonials from "@/components/sections/Testimonials";
 import HowItWorksSection from "@/components/sections/HowItWorks";
 import FeaturePage from "./features/page";
 
@@ -11,7 +10,6 @@ const Home = () => {
       <Hero />
       <HowItWorksSection />
       <FeaturePage />
-      <Testimonials />
       <div className="px-4 md:px-[6rem] flex flex-col gap-[5rem] md:gap-[12.5rem] py-[2rem] md:py-[5rem]">
         <FaqSection />
       </div>
